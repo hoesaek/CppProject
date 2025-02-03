02a1a93eff10c7ae221c542dea40bab27518b53f
hoesaek <102666896+hoesaek@users.noreply.github.com>
2025-02-03 15:29:01 +0100

    Update Exo-Cnam.cpp
    Ajout aléatoire de décorations, et correction de la syntaxe + documentation du code

--- TP_Sapin-Cnam/Exo-Cnam.cpp
+++ TP_Sapin-Cnam/Exo-Cnam.cpp
@@ -1,39 +1,70 @@
-// Exo-Cnam.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 #include <iostream>
-#include <string>
-#include <random>
-#include <time.h>
- 
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
+
 int main()
 {
-	int nbLigne; /*Nombre de ligne du sapin */
-	int nbesp;	/*Nombre d'espace avant '*' */
-	int count; /*compte le nbr de '*' de la derniere ligne */
-	int i;	  /*Compteur de ligne*/
-	int j;   /*Determine emplacement avec nbesp du char '*' */
+    int nbLigne; /* Nombre de ligne du sapin */
+    int nbesp;   /* Nombre d'espaces avant '*' */
+    int count;   /* Nombre de '*' sur la dernière ligne */
+    int i, j;    /* Compteurs */
 
-	//rand() % ... pour delimité rand exemple pour  i =0 i<5 i++ cout rand%5 output 2 4 1 3 5
-	cout << "Veuillez indiquer le nombre de ligne pour le sapin :" << endl; cin >> nbLigne;
-	for (i = 0; i < nbLigne; i++)
-	{
-		nbesp = nbLigne - i - 1;
-		for (j = 0; j < nbesp + 1; j++) putchar(' '); /*nb espaces avant '*' */
-		for (j = 0; j < (2 * (i + 1)) - 1; j++)
-		{
-			if (i > 0) putchar('^'); else putchar('*');
-		}putchar('\n');
-		
-	}
-	count = (nbLigne * 2) - 1;
-	for (i = 0; i < count; ++i)
-	{
-		int a = count * 1 / 3; int b = count * 2 / 3;
-		if ((i < a + 1) || (i >= b + 1)) putchar(' '); else putchar('|');
-	}
-	putchar('\n'); cout << endl;
-	return EXIT_SUCCESS;
+    // Initialisation du générateur de nombres aléatoires
+    srand(time(0));
+
+    cout << "Veuillez indiquer le nombre de lignes pour le sapin : ";
+    cin >> nbLigne;
+
+    // Construction du sapin
+    for (i = 0; i < nbLigne; i++)
+    {
+        nbesp = nbLigne - i - 1;
+
+        // Affichage des espaces
+        for (j = 0; j < nbesp + 1; j++)
+            putchar(' ');
+
+        // Affichage des caractères du sapin avec décorations aléatoires
+        for (j = 0; j < (2 * (i + 1)) - 1; j++)
+        {
+            if (i > 0)
+            {
+                // Ajout aléatoire de décorations
+                int randomNum = rand() % 10;
+                if (randomNum < 2)
+                    putchar('o'); // Boules de Noël
+                else if (randomNum == 3)
+                    putchar('@'); // Étoile spéciale
+                else
+                    putchar('^'); // Feuillage
+            }
+            else
+            {
+                putchar('*'); // Sommet du sapin
+            }
+        }
+        putchar('\n');
+    }
+
+    // Calcul de la largeur du tronc
+    count = (nbLigne * 2) - 1;
+
+    // Affichage du tronc
+    for (i = 0; i < count; ++i)
+    {
+        int a = count / 3;
+        int b = (count * 2) / 3;
+
+        if ((i < a + 1) || (i >= b + 1))
+            putchar(' ');
+        else
+            putchar('|');
+    }
+    
+    putchar('\n');
+    cout << endl;
+
+    return EXIT_SUCCESS;
 }
-/*
-	À finir mettre aleatoirement des decorations sur sapin evite que ce soit moche et pense à utiliser la methode rand() vu en commentaire plutot.
-*/
